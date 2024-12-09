ad077a31a06265de20ba83d551d459c398f2532b
odelola_solomon <odelolasolomon5@gmail.com>
2024-12-09 11:45:58 +0100

    added and pused the ongiong chatbot to the repo

---
+++ chatbot_project/chatbot/views.py
@@ -0,0 +1,22 @@
+# Create your views here.
+from django.shortcuts import render
+from django.http import JsonResponse
+from openai import OpenAI
+import os
+
+def chatbot_response(request):
+    if request.method == 'POST':
+        client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+        user_input = request.POST.get('user_input')
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_input,
+                }
+            ],
+            model="gpt-3.5-turbo",
+        )
+        chatbot_reply = response.choices[0].message.content 
+        return JsonResponse({'reply': chatbot_reply})
+    return render(request, 'chat.html')
